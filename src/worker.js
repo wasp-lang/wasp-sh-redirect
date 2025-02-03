a10982ecda18ebeaef245897c91582c6ea3f96f2
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-02-03 11:48:01 +0100

    Update Wrangler version. Cleanup.

--- src/worker.js
+++ src/worker.js
@@ -1,22 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
-		const base = 'https://wasp-lang.dev';
+		// TODO: Change to wasp.sh when we decide to go live
+		// this is now just testing the redirect from wasp.sh to wasp-lang.dev
+		const redirectToBase = 'https://wasp-lang.dev';
 		const statusCode = 301;
 
 		const url = new URL(request.url);
 		const { pathname, search } = url;
 
-		const destinationURL = `${base}${pathname}${search}`;
+		const destinationURL = `${redirectToBase}${pathname}${search}`;
 
 		return Response.redirect(destinationURL, statusCode);
 	},
